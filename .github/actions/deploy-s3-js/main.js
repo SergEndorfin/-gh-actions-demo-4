f472cd971754d392466f12745692a71abc2f7791
Sergii <sergendorfin@gmail.com>
2026-08-30 23:43:41 +0200

    added S3 deployment into custom JS Action: update JS file: fix 05

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -10,7 +10,7 @@ function run() {
 
     //upload files
     const s3Uri = `s3://${bucketName}`;
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --sse aws:kms`);
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --sse AES256`);
     core.notice('========');
 }
 
